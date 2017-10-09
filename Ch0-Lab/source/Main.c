498034d6de8846d0c77e060fd794f9d4af9e5b81
LaiJunZhi <daleliao1141@gmail.com>
2017-10-09 12:57:30 +0800

    lab 0-2

---
+++ Ch0-Lab/source/Main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	printf("Welcome to C!\n");
+	printf("My student id is:106360217\n");
+	printf("My name is LaiJunZhi\n");
+	system("pause");
+	return 0;
+}
